11360e5da27fb447cd430bbb5417749a6808e391
kadamrohit1103 <90898062+kadamrohit1103@users.noreply.github.com>
2025-12-06 15:21:53 +0530

    Update pdf_text_replacer.bat

--- pdf_text_replacer.bat
+++ pdf_text_replacer.bat
@@ -1,9 +1,9 @@
-set "LOGDIR=D:\kadam\Documents\pdf_find_replace_by_coordinates\terminal_print_logs"
+set "LOGDIR=D:\kadam\Documents\pdf_find_n_replace_by_coordinates\terminal_print_logs"
 set "LOG=%LOGDIR%\pdf_text_replacer.txt"
 set "TMP=%LOGDIR%\pdf_text_replacer.new"
 set "RUN=%LOGDIR%\_"
 
-python "D:\kadam\Documents\pdf_find_replace_by_coordinates\replacement_engine\pdf_text_replacer.py" --config replacements.json --verbose >"%RUN%" 2>&1 && (
+python "D:\kadam\Documents\pdf_find_n_replace_by_coordinates\replacement_engine\pdf_text_replacer.py" --config replacements.json --verbose >"%RUN%" 2>&1 && (
   rem New log = latest run + blank line + old log
   if exist "%TMP%" del "%TMP%"
   type "%RUN%" > "%TMP%"
@@ -14,3 +14,4 @@ python "D:\kadam\Documents\pdf_find_replace_by_coordinates\replacement_engine\pd
   del "%RUN%"
   notepad "%LOG%"
 )
+
