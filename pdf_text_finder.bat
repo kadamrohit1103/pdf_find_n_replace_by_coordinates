b77a59450d6fdbedaab5e371cdb4d53070716df6
kadamrohit1103 <90898062+kadamrohit1103@users.noreply.github.com>
2025-12-06 15:21:06 +0530

    Update pdf_text_finder.bat

--- pdf_text_finder.bat
+++ pdf_text_finder.bat
@@ -1,7 +1,7 @@
-set "LOG=D:\kadam\Documents\pdf_find_replace_by_coordinates\terminal_print_logs\pdf_text_finder.txt"
+set "LOG=D:\kadam\Documents\pdf_find_n_replace_by_coordinates\terminal_print_logs\pdf_text_finder.txt"
 set "TMP=%LOG%.new"
 
-python "D:\kadam\Documents\pdf_find_replace_by_coordinates\replacement_engine\pdf_text_finder.py" --config finder_config.json >_ 2>&1 && (
+python "D:\kadam\Documents\pdf_find_n_replace_by_coordinates\replacement_engine\pdf_text_finder.py" --config finder_config.json >_ 2>&1 && (
   rem build new log = new output + blank line + old log (if exists)
   if exist "%TMP%" del "%TMP%"
   type _ > "%TMP%"
@@ -11,3 +11,4 @@ python "D:\kadam\Documents\pdf_find_replace_by_coordinates\replacement_engine\pd
   del _
   notepad "%LOG%"
 )
+
